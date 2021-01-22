e595bf1b2460e23f55ea3f3464ad4f69fe730e82
Shisato <shisatoyano@gmail.com>
2021-01-22 21:52:51 +0900

    define start/goal state

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+float deg2rad(float deg)
+{
+    float rad;
+
+    return rad = deg * M_PI / 180.0;
+}
+
 int main()
 {
-    cout << "Hello world" << endl;
+    // start state
+    float start_x = 1.0; // [m]
+    float start_y = 1.0; // [m]
+    float start_yaw = deg2rad(45.0); // [rad]
+
+    // goal state
+    float goal_x = -3.0; // [m]
+    float goal_y = -3.0; // [m]
+    float goal_yaw = deg2rad(-45.0); // [rad]
+
+    float curvature = 1.0;
 
     return 0;
 }
